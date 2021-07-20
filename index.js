0bb05c58d4e1b5964965675301206bd53248f988
Matt <mbrowne108@gmail.com>
2021-07-20 18:00:11 -0400

    commit tree 32d2164fe63df62a6035372134b9d4f5f2d5f634

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun();
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair='*') {
+    return function(compliment="special") {
+        return `You are ${flair}${compliment}${flair}!`;
+    }
+}
+
+wrapAdjective("%")("a hard worker")
+
